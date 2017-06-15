c770e94a7a8546e1297f4a097a762c799fc8d2ba
Casey Kwok <casey.kwok@harborx.com>
2017-06-15 09:59:43 -0700

    Add setTimeout() to delay reset screen and minor edits to CSS

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,14 @@
-var userName = prompt("What's your name?")
-alert("Hello, " + userName + "! Let's see how many Disney movies you know. Good luck!")
+function greetings() {
+	var userName = prompt("What's your name?");
+	if (userName === "") {
+		alert("Please enter your name.");
+		greetings()
+	} else {
+		alert("Hello, " + userName + "! Let's see how many Disney movies you know. Good luck!");
+	}
+}
+
+greetings()
 
 var allSolutions = ["aladdin", "thelionking", "frozen", "mulan", "beautyandthebeast", "cinderella", "thelittlemermaid", "tangled", "up", "findingdory", "findingnemo", "despicableme", "moana", "liloandstitch", "zootopia", "toystory", "monstersinc"];
 var audioImageFact = {
@@ -29,6 +38,13 @@ function reset() {
 
 reset()
 
+function updateScreen() {
+	document.getElementById("wins").innerHTML = wins;
+	document.getElementById("current_word").innerHTML = currentWord.join(" ");
+	document.getElementById("remaining_guesses").innerHTML = lives;
+	document.getElementById("guesses").innerHTML = allUserGuesses.join(", ");
+}
+
 document.onkeyup = function(event) {
 	var userGuess = event.key;
 	// console.log(userGuess)
@@ -38,11 +54,11 @@ document.onkeyup = function(event) {
 				// console.log(i)
 				if (userGuess === solutionCharacters[i]) {
 					currentWord.fill(userGuess, i, i + 1);
-					// console.log(currentWord);
+					console.log(currentWord.join(" "));
 					if (currentWord.join("") === solution) {
 						wins++;
-						alert("Congratulations, you got it right! The answer is " + solution + ". Keep going!");
-						reset();
+						setTimeout(function() { alert("Congratulations, you got it right! The answer is " + solution + ". Keep going!");
+						reset(); }, 500);
 					}
 				}
 			}
@@ -50,13 +66,10 @@ document.onkeyup = function(event) {
 			allUserGuesses.push(userGuess);
 			lives--;
 			if (lives === 0) {
-				alert("Aw, bummer! You ran out of guesses. It's okay, keep trying!");
-				reset();
+				setTimeout(function() { alert("Aw, bummer! You ran out of guesses. It's okay, keep trying!");
+				reset(); }, 500);
 			}
 		}
 	}
-	document.getElementById("wins").innerHTML = wins;
-	document.getElementById("current_word").innerHTML = currentWord.join(" ");
-	document.getElementById("remaining_guesses").innerHTML = lives;
-	document.getElementById("guesses").innerHTML = allUserGuesses.join(", ");
+	updateScreen();
 }
