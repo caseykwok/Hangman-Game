a043978fa41b2cbe2650ea2ea04a9329b12c9f70
Casey Kwok <casey.kwok@harborx.com>
2017-06-15 17:26:32 -0700

    Change source of iFrame instead of changing entire iFrame

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,11 +12,11 @@ greetings()
 
 var allSolutions = ["aladdin", "the lion king", "frozen", "mulan", "beauty and the beast", "cinderella", "the little mermaid", "tangled", "up", "finding dory", "finding nemo", "despicable me", "moana", "lilo and stitch", "zootopia", "toy story", "monsters inc"];
 var audioImageFact = {
-	up: ["<iframe width='100%' height='300' scrolling='no' frameborder='no' src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/117267971&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true'></iframe>", "url('assets/images/picture_1.jpg')", "There are 10,286 balloons attached to the house in Up."], 
-	findingDory: ["<iframe width='100%' height='300' scrolling='no' frameborder='no' src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/154004102&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true'></iframe>", "url('assets/images/picture_2.jpg')", "A character from Inside Out also makes an appearance in Finding Dory."], 
-	theLionKing: ["<iframe width='100%' height='300' scrolling='no' frameborder='no' src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/20830844&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true'></iframe>", "url('assets/images/picture_3.jpg')", "Pumbaa was the first character to fart in a Disney movie."], 
-	despicableMe: ["<iframe width='100%' height='300' scrolling='no' frameborder='no' src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/99800401&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true'></iframe>", "url('assets/images/picture_4.jpg')", "The average Minion stands at 105cm in height."], 
-	moana: ["<iframe width='100%' height='300' scrolling='no' frameborder='no' src='https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/314001417&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true'></iframe>", "url('assets/images/picture_5.jpg')", "Flounder from The Little Mermaid and Olaf from Frozen both appear in the film Moana."]
+	up: ["https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/117267971&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=false", "url('assets/images/picture_1.jpg')", "There are 10,286 balloons attached to the house in Up."], 
+	findingDory: ["https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/154004102&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=false", "url('assets/images/picture_2.jpg')", "A character from Inside Out also makes an appearance in Finding Dory."], 
+	theLionKing: ["https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/20830844&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=false", "url('assets/images/picture_3.jpg')", "Pumbaa was the first character to fart in a Disney movie."], 
+	despicableMe: ["https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/99800401&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=false", "url('assets/images/picture_4.jpg')", "The average Minion stands at 105cm in height."], 
+	moana: ["https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/314001417&amp;auto_play=true&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=false", "url('assets/images/picture_5.jpg')", "Flounder from The Little Mermaid and Olaf from Frozen both appear in the film Moana."]
 }
 // need audioImageFactValues because Object.values() does not work in Safari
 var audioImageFactValues = Object.keys(audioImageFact).map(function(key) {
@@ -36,7 +36,8 @@ function reset() {
 	document.getElementById("current_word").innerHTML = currentWord.join(" ");
 	document.getElementById("remaining_guesses").innerHTML = lives;
 	document.getElementById("guesses").innerHTML = allUserGuesses.join(", ");
-	document.getElementById("audio").innerHTML = theme[0];
+	// document.getElementById("audio").innerHTML = theme[0];
+	document.getElementById("myiFrame").setAttribute('src', theme[0]); 
 	document.getElementById("background_image").style.backgroundImage = theme[1];
 	document.getElementById("fun_fact").innerHTML = theme[2];
 }
